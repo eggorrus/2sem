57300a3964beae04f8c79121dfd55e160035376f
eggorrus <egoryar06@gmail.com>
2025-02-19 13:11:29 +0300

    Исправил обработку исключений, обратил внимание на замечания ревьюверов
    1. Обработка ошибок реализована через оператор throw;
    2. Добавлена xml-документация;
    3. Реализован деструктор класса Stack;
    4. Значения указателя top и переменной size перенесены в зону public

--- 2sem/common/stack.h
+++ 2sem/common/stack.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
 template <typename T>
 
+/// <summary>
+/// Ниже находится описание класса "Stack", который является системой хранения данных типа "LIFO"
+/// Description of class "Stack" which is data storage "LIFO" system is below.
+/// </summary>
 class Stack
 {
 private:
@@ -10,10 +19,37 @@ private:
 		T data;
 		Node* next;
 	};
-	Node* top = nullptr;
-	int size = 0;
 
 public:
+	/// <summary>
+	/// Используем в публичной части указатель на вершину стека, по умолчанию являющийся nullptr, а также вводим целочисленную переменную, отвечающую за размер стека.
+	/// In the public part, we use a pointer to the top of the stack, which is nullptr by default, and also enter an integer variable responsible for the stack size.
+	/// </summary>
+	Node* top = nullptr;
+	int size = 0;
+	/// <summary>
+	/// Здесь представлен деструктор.
+	/// Here is destructor
+	/// </summary>
+	~Stack()
+	{
+		clear();
+	}
+	/// <summary>
+	/// Далее представлены следующие методы для работы со стеком:
+	/// void push(T)* - добавляет элемент на вершину стека
+	/// T pop() * -извлекает элемент с вершины стека и возвращает его значение
+	/// T peek() * -возвращает элемент находящийся на вершине стека
+	/// int count() * -возвращает количество элементов в коллекции
+	/// void clear() * -удаляет все элементы из коллекции
+	/// The following methods are presented for working with the stack:
+	/// void push(T)* - adds an element to the top of the stack
+	/// T pop() * -retrieves an element from the top of the stack and returns its value
+	/// T peek() * -returns the element at the top of the stack
+	/// int count() * -returns the number of items in the collection
+	/// void clear() * -removes all items from the collection
+	/// </summary>
+	/// <param name="value"></param>
 	void push(const T& value)
 	{
 		Node* newData = new Node();
@@ -28,13 +64,15 @@ public:
 		if (top != nullptr)
 		{
 			T topValue = top->data;
+			Node* temp = top;
 			top = top->next;
+			delete temp;
 			--size;
 			return topValue;
 		}
 		else
 		{
-			std::cerr << "Stack is empty\n";
+			throw runtime_error("Stack is empty");
 		}
 	}
 
@@ -46,7 +84,7 @@ public:
 		}
 		else
 		{
-			std::cerr << "Stack is empty\n";
+			throw runtime_error("Stack is empty");
 		}
 	}
 
